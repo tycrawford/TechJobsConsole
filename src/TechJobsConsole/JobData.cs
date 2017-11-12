d89af0b5f410d65796dfc40862903006ebf04b52
Ty Crawford <tycrawford314@gmail.com>
2017-11-12 00:13:39 -0600

    Stuck working on the FindByValue function, seems to be returning too much data. The PrintJobs function appears to work fine though, as does everything up to that point.

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -7,13 +7,13 @@ namespace TechJobsConsole
 {
     class JobData
     {
-        static List<Dictionary<string, string>> AllJobs = new List<Dictionary<string, string>>();
+        static List<Dictionary<string, string>> AllJobs = new List<Dictionary<string, string>>(); //builds AllJobs, a List of str, stri Dictos
         static bool IsDataLoaded = false;
 
-        public static List<Dictionary<string, string>> FindAll()
+        public static List<Dictionary<string, string>> FindAll() //this is used to recall literally all data
         {
-            LoadData();
-            return AllJobs;
+            LoadData();         //runs LoadData, just checks to make sure .csv is loaded
+            return AllJobs;     //since we're not picking and pulling, this should just return all loaded data
         }
 
         /*
@@ -24,18 +24,18 @@ namespace TechJobsConsole
         {
             LoadData();
 
-            List<string> values = new List<string>();
+            List<string> values = new List<string>(); //new list of just strings called Values
 
-            foreach (Dictionary<string, string> job in AllJobs)
+            foreach (Dictionary<string, string> job in AllJobs) 
             {
-                string aValue = job[column];
-
-                if (!values.Contains(aValue))
-                {
+                string aValue = job[column]; //uses inhereted 'column' passed in to return the value associated with that key
+                                             
+                if (!values.Contains(aValue))//if our new list of just plain, non-repeated values does not contain this iterations value
+                {                            //then add that value to the list of values
                     values.Add(aValue);
-                }
+                }                            //otherwise it is already in the list and we dont want it to be repeated
             }
-            return values;
+            return values;                   //send the list<str> back to the function
         }
 
         public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)
@@ -43,11 +43,11 @@ namespace TechJobsConsole
             // load data, if not already loaded
             LoadData();
 
-            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
-
-            foreach (Dictionary<string, string> row in AllJobs)
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>(); //creates a new list of str, str dictos
+            
+            foreach (Dictionary<string, string> row in AllJobs) //looks at all jobs/rows in AllJobs
             {
-                string aValue = row[column];
+                string aValue = row[column]; 
 
                 if (aValue.Contains(value))
                 {
@@ -58,45 +58,71 @@ namespace TechJobsConsole
             return jobs;
         }
 
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData(); //checks to make sure jobs are loaded
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>(); //creates new variable, list of dictionaries with string/string pairs
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                bool hasGoodData = false;
+                foreach (KeyValuePair<string, string> field in row)
+                {
+                    string aValue = field.Value;
+                    if (aValue.Contains(value))
+                    {
+                        hasGoodData = true;
+                        
+                    }
+
+                }
+                jobs.Add(row);
+            }
+
+
+
+            return jobs;
+        }
         /*
          * Load and parse data from job_data.csv
          */
         private static void LoadData()
         {
 
-            if (IsDataLoaded)
+            if (IsDataLoaded) //if data is already laoded, no need in loading it again
             {
-                return;
+                return; //bypasses rest of function
             }
 
-            List<string[]> rows = new List<string[]>();
-
-            using (StreamReader reader = File.OpenText("job_data.csv"))
+            List<string[]> rows = new List<string[]>(); //buids a list of string arrays called Rows
+                                                        //each array will be a row, each string will be a cell
+            using (StreamReader reader = File.OpenText("job_data.csv")) //this is where we actually load the file
             {
-                while (reader.Peek() >= 0)
+                while (reader.Peek() >= 0) //honestly unsure whats going on with this conditional
                 {
-                    string line = reader.ReadLine();
-                    string[] rowArrray = CSVRowToStringArray(line);
-                    if (rowArrray.Length > 0)
+                    string line = reader.ReadLine(); //builds string called line from a line in csv
+                    string[] rowArrray = CSVRowToStringArray(line); //converts CSVRow to an array now called rowArray
+                    if (rowArrray.Length > 0) //checking to make sure a row is actually good
                     {
-                        rows.Add(rowArrray);
+                        rows.Add(rowArrray); //appends the rows list with the next row
                     }
                 }
             }
 
-            string[] headers = rows[0];
-            rows.Remove(headers);
+            string[] headers = rows[0]; //creates an array of strings called Headers, just equal to first array in rows list, 
+            rows.Remove(headers); //uses headers to eliminate itself from the list of arrays
 
             // Parse each row array into a more friendly Dictionary
-            foreach (string[] row in rows)
+            foreach (string[] row in rows) //begins converting list of arrays to dicto
             {
-                Dictionary<string, string> rowDict = new Dictionary<string, string>();
+                Dictionary<string, string> rowDict = new Dictionary<string, string>(); //builds dicto rowDict
 
-                for (int i = 0; i < headers.Length; i++)
+                for (int i = 0; i < headers.Length; i++) //for each column, as defined by the length of the headers row
                 {
-                    rowDict.Add(headers[i], row[i]);
-                }
-                AllJobs.Add(rowDict);
+                    rowDict.Add(headers[i], row[i]); //adds a key value pair of the headers and their respective rows. 
+                }                                    //the significance of this, i believe, is that we can say, show me all of the
+                AllJobs.Add(rowDict);                //X header, like location, and it will return a value of all cells in that column
             }
 
             IsDataLoaded = true;
--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -28,23 +28,24 @@ namespace TechJobsConsole
             while (true)
             {
 
-                string actionChoice = GetUserSelection("View Jobs", actionChoices);
-
-                if (actionChoice.Equals("list"))
+                string actionChoice = GetUserSelection("View Jobs", actionChoices); //sends dicto and header string to G.U.S. function
+                                                                                    //returns string of key
+                if (actionChoice.Equals("list"))                                    //if key string is list
                 {
-                    string columnChoice = GetUserSelection("List", columnChoices);
+                    string columnChoice = GetUserSelection("List", columnChoices);  //run the G.U.S. again but for column choices
 
-                    if (columnChoice.Equals("all"))
+                    if (columnChoice.Equals("all"))                                 //if returned string is all
                     {
-                        PrintJobs(JobData.FindAll());
+                        PrintJobs(JobData.FindAll());                               //run jobData.FindAll() **note not FindAll(string) function
                     }
                     else
                     {
-                        List<string> results = JobData.FindAll(columnChoice);
+                        List<string> results = JobData.FindAll(columnChoice);       //runs a function that will return a list<str> of all different entries
+                                                                                    //in that column
 
-                        Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");
-                        foreach (string item in results)
-                        {
+                        Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");  //this prints a sort of header, users the columnChoices dicto spit out a nicely formatted header
+                        foreach (string item in results)                            //iterates over all entries in the list
+                        {                                                           //simply writes each entry in list
                             Console.WriteLine(item);
                         }
                     }
@@ -58,17 +59,18 @@ namespace TechJobsConsole
                     Console.WriteLine("\nSearch term: ");
                     string searchTerm = Console.ReadLine();
 
-                    List<Dictionary<string, string>> searchResults;
+                    List<Dictionary<string, string>> searchResults; //creates a new list of <str><Str> dictos called searchResults
 
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
                     }
                     else
                     {
-                        searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);
-                        PrintJobs(searchResults);
+                        searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm); //takes in columnChoice Key from GSU function, as well as the search term
+                        PrintJobs(searchResults); //takes data back from FBCAV above, list of str str dictos, and runs that list back through print jobs
                     }
                 }
             }
@@ -78,46 +80,67 @@ namespace TechJobsConsole
          * Returns the key of the selected item from the choices Dictionary
          */
         private static string GetUserSelection(string choiceHeader, Dictionary<string, string> choices)
-        {
+        {//literally all this function does is spit out a good key that's usable in other dictionaries, all based on numerical user input
             int choiceIdx;
             bool isValidChoice = false;
-            string[] choiceKeys = new string[choices.Count];
+            string[] choiceKeys = new string[choices.Count]; //initializes array of X number slots, X equal to number of choices
 
             int i = 0;
-            foreach (KeyValuePair<string, string> choice in choices)
-            {
-                choiceKeys[i] = choice.Key;
-                i++;
-            }
-
+            foreach (KeyValuePair<string, string> choice in choices)//for each entry in inhereted dicto "choices"
+            {                                                       //the choiceKeys slot at index i gets filled
+                choiceKeys[i] = choice.Key;                         //with the same spot key in the dicto
+                i++;                                                //this just gives us a separate array
+            }                                                       //of just keys, and we can use the number
+                                                                    //input by the user to correspond to a dicto key
             do
             {
-                Console.WriteLine("\n" + choiceHeader + " by:");
+                Console.WriteLine("\n" + choiceHeader + " by:");    //NOTE choice header inhereted
 
-                for (int j = 0; j < choiceKeys.Length; j++)
-                {
-                    Console.WriteLine(j + " - " + choices[choiceKeys[j]]);
-                }
-
-                string input = Console.ReadLine();
-                choiceIdx = int.Parse(input);
-
-                if (choiceIdx < 0 || choiceIdx >= choiceKeys.Length)
-                {
+                for (int j = 0; j < choiceKeys.Length; j++)                 //setting up iteration
+                {                                                                   
+                    Console.WriteLine(j + " - " + choices[choiceKeys[j]]);  //this just outputs a list of choices
+                }                                                           //as well as their indexed numbers for better choice making
+                    
+                string input = Console.ReadLine();                          //takes user input and assigns to input string variable
+                choiceIdx = int.Parse(input);                               //parses input into the choiceIdx variable set earlier
+                                                                            //should also throw an error if it cannot parse as an int
+                if (choiceIdx < 0 || choiceIdx >= choiceKeys.Length)        //allows only numbers within range to be selected
+                {                                                           //throws errors otherwise
                     Console.WriteLine("Invalid choices. Try again.");
                 }
                 else
                 {
-                    isValidChoice = true;
+                    isValidChoice = true;                                   //sets validchoice to true, breaks loop
                 }
 
             } while (!isValidChoice);
 
-            return choiceKeys[choiceIdx];
-        }
+            return choiceKeys[choiceIdx]; //having selected an index in choiceIdx, and using that index to gain a choice from choiceKeys
+        }                                 //we can now return the appropriate key to the function that called this function
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
-        {
+        {//here is step one, implement this function
+            int numboDicto = someJobs.Count;
+            if (numboDicto == 0)
+            {
+                Console.WriteLine("The requested data does not exist or turned up no results");
+            
+            }
+            else
+            {
+                Console.WriteLine("Jobs:");
+                foreach (Dictionary<string, string> dicto in someJobs)
+                {
+                    Console.WriteLine("*****");
+                    foreach (KeyValuePair<string, string> job in dicto)
+                    {
+                        Console.Write(job.Key + ": ");
+                        Console.Write(job.Value);
+                        Console.Write('\n');
+                    }
+                    Console.WriteLine("*****\n");
+                }
+            }
             Console.WriteLine("printJobs is not implemented yet");
         }
     }
